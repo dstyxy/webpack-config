263554d2a9d1b14d7e77dcfee0f6b1d1a2fc6e2e
dstyxy <dstyxy@qq.com>
2019-03-13 17:30:29 +0800

    初始化

---
+++ build/webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  context: path.resolve(__dirname, '../'),
+  entry: {
+    main: './src/main.js'
+  },
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "[name].[hash].js",
+    publicPath:'/'
+  },
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, '../src'),
+    }
+  },
+  devtool:'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [ 
+          'style-loader', 
+          'css-loader' ,
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.styl(us)?$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'stylus-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              outputPath: 'images/'
+            }
+          }
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    })
+  ],
+  devServer: {
+    contentBase: false,
+    compress: false,
+    inline: true,
+    hot: true,
+    quiet: true,
+    port: 9000,
+    clientLogLevel: 'warning',
+    publicPath: '/',
+    watchOptions: {
+      poll: false
+    }
+  },
+}
---
+++ src/main.js
@@ -0,0 +1,12 @@
+let app = document.querySelector('#app')
+
+import log from '@/assets/images/log.png'
+
+app.innerHTML = 'webpack'
+import '@/assets/css/reset.css'
+import '@/assets/stylus/reset.stylus'
+
+let img = document.createElement('img')
+img.src = log
+
+document.body.appendChild(img)
